ae0170a6e936477bae15f65c459832d2916146d9
Anson <git@anson-cheung.com>
2018-07-30 01:03:40 +0800

    Update demo to use Scrollable object and fix bugs

--- reactiontest.py
+++ reactiontest.py
@@ -1,7 +1,9 @@
 import discord
 from modules.botModule import *
+from tinydb import TinyDB, Query
 import shlex
 import time
+import math
 
 class Scrollable():
     # This object provides an easy way to turn a TinyDB table into a scrollable embed.
@@ -13,25 +15,27 @@ class Scrollable():
         self.title = title
         self.inline = inline
         self.embeds = []
-        self.preprocessed_data = []
-        self.refresh()
+        self.processed_data = []
 
     def preprocess(self, field):
         # By default this method will take the designated field and make a title, omitting 'description'.
         # You may want to override this default behaviour to suit your table's needs.
         # The resulting embeds will be created with the order than the list is returned in.
         # Should return a 2-dimensional list of [title, data].
-        self.processed_data = [[x[field], ''] for x in self.table]
+        self.processed_data = [[x[field], 'Testing'] for x in self.table]
 
     def refresh(self, *, field):
-        self.processed_data.clear
-        self.embeds.clear
-        self.preprocess(self, field=field)
+        self.processed_data.clear()
+        self.embeds.clear()
+        self.preprocess(field=field)
         counter = 1
+        page = 1
         for item in self.processed_data:
+            page = math.ceil(counter/self.limit)
             if counter == 1:
                 # Just started the loop -- make a new embed object
                 embed = discord.Embed(title=self.title, color=self.color)
+                embed.set_footer(text="Page " + str(page) + " of " + str(math.ceil(len(self.processed_data)/self.limit)))
                 embed.add_field(name=item[0], value=item[1], inline=self.inline)
             elif counter % self.limit == 0:
                 # Last one in this embed object, now append and make a new object
@@ -39,21 +43,25 @@ class Scrollable():
                 self.embeds.append(embed)
                 del embed
                 embed = discord.Embed(title=self.title, color=self.color)
-            elif counter == len(self.preprocessed_data):
+                embed.set_footer(text="Page " + str(page+1) + " of " + str(math.ceil(len(self.processed_data)/self.limit)))
+            elif counter == len(self.processed_data):
                 # Last one in loop
                 embed.add_field(name=item[0], value=item[1], inline=self.inline)
                 self.embeds.append(embed)
                 del embed
             else:
                 # Else, nothing special needs to be done.
-                embed = discord.Embed(title=self.title, color=self.color)
+                embed.add_field(name=item[0], value=item[1], inline=self.inline)
             counter += 1
 
     def next(self, *, current_pos):
-        return self.embeds[(current_pos + 1) % self.limit]
+        return self.embeds[(int(current_pos) + 1) % len(self.embeds)]
 
     def previous(self, *, current_pos):
-        return self.embeds[(current_pos - 1) % self.limit]
+        return self.embeds[(int(current_pos) - 1) % len(self.embeds)]
+
+    def initial_embed(self):
+        return self.embeds[0]
 
 class ReactionTest(BotModule):
     name = 'reactiontest'
@@ -68,36 +76,53 @@ class ReactionTest(BotModule):
 
     listen_for_reaction = True
 
-
     # Instead of using TinyDB, since scubot can't work with reactions persistently a list should be used
     message_returns = []
 
     async def contains_returns(self, message):
         for x in self.message_returns:
-            if message.id == x.id:
+            if message.id == x[0].id:
                 return True
         return False
 
+    async def find_pos(self, message):
+        for x in self.message_returns:
+            if message.id == x[0].id:
+                return x[1]
+
+    async def update_pos(self, message, ty):
+        for x in self.message_returns:
+            if message.id == x[0].id:
+                if ty == 'next':
+                    x[1] += 1
+                if ty == 'prev':
+                    x[1] -= 1
+
     async def parse_command(self, message, client):
-        embed = discord.Embed(title="Reaction based scrolling demo", color=0x000000)
-        embed.add_field(name="One", value="Hello there!", inline=True)
-        message_return = await client.send_message(message.channel, embed=embed)
-        self.message_returns.append(message_return)
+        self.module_db.purge()
+        for x in range(1, 16):
+            self.module_db.insert({'foo': x})
+        self.scroll = Scrollable(limit=4, color=0xc0fefe, table=self.module_db, title="Demo", inline=True)
+        self.scroll.refresh(field='foo')
+        message_return = await client.send_message(message.channel, embed=self.scroll.initial_embed())
+        self.message_returns.append([message_return, 0])
         await client.add_reaction(message_return, "⏪")
         await client.add_reaction(message_return, "⏩") # Ugh
-        # TODO: change the addition of reactions into a loop
 
     # Now we hand off to the reaction command
 
     async def on_reaction_add(self, reaction, client, user):
         if not await self.contains_returns(reaction.message):
             return 0
+        pos = await self.find_pos(reaction.message)
         react_text = reaction.emoji
         if type(reaction.emoji) is not str:
             react_text = reaction.emoji.name
-        if react_text == "⏪" or react_text == "⏩":
-        # TODO: differentiate between different react_texts
-            embed = discord.Embed(title="Reaction based scrolling demo", color=0x000000)
-            embed.add_field(name="Two", value="General Kenobi!", inline=True)
-            # TODO: be able to dynamically change what is edited
+        if react_text == "⏩":
+            embed = self.scroll.next(current_pos=pos)
             await client.edit_message(reaction.message, embed=embed)
+            await self.update_pos(reaction.message, 'next')
+        if react_text == "⏪":
+            embed = self.scroll.previous(current_pos=pos)
+            await client.edit_message(reaction.message, embed=embed)
+            await self.update_pos(reaction.message, 'prev')
