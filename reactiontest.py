cea055178c9b422c8b4f70c13ea80a41836dec84
Anson <git@anson-cheung.com>
2018-07-29 23:32:33 +0800

    Add scrollable object (untested)

--- reactiontest.py
+++ reactiontest.py
@@ -3,6 +3,58 @@ from modules.botModule import *
 import shlex
 import time
 
+class Scrollable():
+    # This object provides an easy way to turn a TinyDB table into a scrollable embed.
+    # However, scrolling logic is not included in this object.
+    def __init__(self, *, limit, color, table, title, inline):
+        self.color = color
+        self.limit = limit
+        self.table = table
+        self.title = title
+        self.inline = inline
+        self.embeds = []
+        self.preprocessed_data = []
+        self.refresh()
+
+    def preprocess(self, field):
+        # By default this method will take the designated field and make a title, omitting 'description'.
+        # You may want to override this default behaviour to suit your table's needs.
+        # The resulting embeds will be created with the order than the list is returned in.
+        # Should return a 2-dimensional list of [title, data].
+        self.processed_data = [[x[field], ''] for x in self.table]
+
+    def refresh(self, *, field):
+        self.processed_data.clear
+        self.embeds.clear
+        self.preprocess(self, field=field)
+        counter = 1
+        for item in self.processed_data:
+            if counter == 1:
+                # Just started the loop -- make a new embed object
+                embed = discord.Embed(title=self.title, color=self.color)
+                embed.add_field(name=item[0], value=item[1], inline=self.inline)
+            elif counter % self.limit == 0:
+                # Last one in this embed object, now append and make a new object
+                embed.add_field(name=item[0], value=item[1], inline=self.inline)
+                self.embeds.append(embed)
+                del embed
+                embed = discord.Embed(title=self.title, color=self.color)
+            elif counter == len(self.preprocessed_data):
+                # Last one in loop
+                embed.add_field(name=item[0], value=item[1], inline=self.inline)
+                self.embeds.append(embed)
+                del embed
+            else:
+                # Else, nothing special needs to be done.
+                embed = discord.Embed(title=self.title, color=self.color)
+            counter += 1
+
+    def next(self, *, current_pos):
+        return self.embeds[(current_pos + 1) % self.limit]
+
+    def previous(self, *, current_pos):
+        return self.embeds[(current_pos - 1) % self.limit]
+
 class ReactionTest(BotModule):
     name = 'reactiontest'
 
