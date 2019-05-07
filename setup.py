490354d3cc1ea580a21eee8a78808ef1ce1b0b9d
hxtk <peter@psanders.me>
2019-05-07 16:15:01 -0400

    Setup file

---
+++ setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="reaction-scroll",
+    version="0.0.1.dev",
+    author="Scubot Team",
+    description="Scrollable Embeds for Discord bots",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/scubot/reaction-scroll",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        "discord"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
+
+
