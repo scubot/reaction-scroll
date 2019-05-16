135fe4353de403f5812a79670deb8a7fc8d09898
hxtk <peter@psanders.me>
2019-05-16 13:14:34 -0400

    Rewrite as self-contained cog

--- reactionscroll/__init__.py
+++ reactionscroll/__init__.py
@@ -1,63 +1,66 @@
-import discord
-import math
+from typing import List, Mapping, Any
 
-class Scrollable():
-    # This object provides an easy way to turn a TinyDB table into a scrollable embed.
-    # However, scrolling logic is not included in this object.
-    def __init__(self, *, limit, color, table, title, inline):
+import discord
+from discord.ext import commands
+
+
+class ScrollView(commands.Cog):
+    def __init__(self, bot, message, embeds, index=0):
+        self.bot = bot
+        self.message = message
+        self.embeds = embeds
+        self.index = index
+
+    @commands.Cog.listener()
+    async def on_reaction_add(self, reaction, user):
+        if reaction.message != self.message:
+            return
+
+        react_text = reaction.emoji
+        if type(reaction.emoji) is not str:
+            react_text = reaction.emoji.name
+        if react_text == "⏩":
+            await reaction.message.edit(embed=self.next())
+        if react_text == "⏪":
+            await reaction.message.edit(embed=self.previous())
+
+    def next(self) -> discord.Embed:
+        self.index = (self.index + 1) % len(self.embeds)
+        return self.embeds[self.index]
+
+    def previous(self, *, current_pos) -> discord.Embed:
+        self.index = (self.index - 1) % len(self.embeds)
+        return self.embeds[self.index]
+
+    def current(self):
+        return self.embeds[self.index]
+
+
+class ScrollViewBuilder:
+    def __init__(self, color, title, inline, bot: commands.Bot):
         self.color = color
-        self.limit = limit
-        self.table = table
         self.title = title
         self.inline = inline
-        self.embeds = []
-        self.processed_data = []
+        self.bot = bot
 
-    def preprocess(self, field):
-        # By default this method will take the designated field and make a title, omitting 'description'.
-        # You may want to override this default behaviour to suit your table's needs.
-        # The resulting embeds will be created with the order than the list is returned in.
-        # Should return a 2-dimensional list of [title, data].
-        return [[x[field], 'Testing'] for x in self.table]
+    def create_with(self, data: List[Mapping[Any, Any]], ctx: commands.Context,
+                    key_str=None,
+                    value_str=None) -> ScrollView:
+        embeds = list()
+        for page in data:
+            embed = discord.Embed(
+                title=self.title,
+                color=self.color)
 
-    def refresh(self, field):
-        self.processed_data.clear()
-        self.embeds.clear()
-        self.processed_data = self.preprocess(field=field)
-        self.create_embeds()
+            for k, v in page.items():
+                k = key_str(k) if key_str else str(k)
+                v = value_str(v) if value_str else str(v)
+                embed.add_field(name=k, value=v)
+            embeds.append(embed)
 
-    def create_embeds(self):
-        counter = 1
-        page = 1
-        for item in self.processed_data:
-            page = math.ceil(counter/self.limit)
-            if counter == 1:
-                # Just started the loop -- make a new embed object
-                embed = discord.Embed(title=self.title, color=self.color)
-                embed.set_footer(text="Page " + str(page) + " of " + str(math.ceil(len(self.processed_data)/self.limit)))
-                embed.add_field(name=item[0], value=item[1], inline=self.inline)
-            elif counter % self.limit == 0:
-                # Last one in this embed object, now append and make a new object
-                embed.add_field(name=item[0], value=item[1], inline=self.inline)
-                self.embeds.append(embed)
-                del embed
-                embed = discord.Embed(title=self.title, color=self.color)
-                embed.set_footer(text="Page " + str(page+1) + " of " + str(math.ceil(len(self.processed_data)/self.limit)))
-            elif counter == len(self.processed_data):
-                # Last one in loop
-                embed.add_field(name=item[0], value=item[1], inline=self.inline)
-                self.embeds.append(embed)
-                del embed
-            else:
-                # Else, nothing special needs to be done.
-                embed.add_field(name=item[0], value=item[1], inline=self.inline)
-            counter += 1
+        message = ctx.send(embed=embeds[0])
 
-    def next(self, *, current_pos):
-        return self.embeds[(int(current_pos) + 1) % len(self.embeds)]
+        view = ScrollView(self.bot, embeds, message)
+        self.bot.add_cog(view)
 
-    def previous(self, *, current_pos):
-        return self.embeds[(int(current_pos) - 1) % len(self.embeds)]
-
-    def initial_embed(self):
-        return self.embeds[0]
+        return view
